0712d16aa62383ba9d6bf2d69ccd59b0b50513e5
evilsimon789 <58799463+evilsimon789@users.noreply.github.com>
2021-12-08 17:22:26 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -42,7 +42,7 @@ def home():
 def deleteEmp():
     print(request.form['delete'], file=sys.stderr)
     print(request.form['delete'])
-    return json.dumps({'success':True, "data": "1"}), 200, {'ContentType':'application/json'} 
+    return json.dumps({'success':True, 'response': '1'}), 200, {'ContentType':'application/json'} 
 
 
 
