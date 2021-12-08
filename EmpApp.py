8e4dc371d5b369e49770b54088832f71acbf5f6b
evilsimon789 <58799463+evilsimon789@users.noreply.github.com>
2021-12-08 20:44:11 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -49,7 +49,7 @@ def deleteEmp():
     return jsonify({ 'response': '1'}) 
 
 @app.route("/userProfile", methods=['POST'])
-def about():
+def userProfile():
     return render_template('GetEmpOutput.html')
 
 @app.route("/about", methods=['POST'])
