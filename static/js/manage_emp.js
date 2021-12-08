9acde3e723080dda5548f81bf35831fa21000d22
evilsimon789 <58799463+evilsimon789@users.noreply.github.com>
2021-12-08 18:25:15 +0800

    Add files via upload

--- static/js/manage_emp.js
+++ static/js/manage_emp.js
@@ -8,55 +8,54 @@ $(document).ready(function(){
 	$(document).on("click","input.delete-btn",function(){
 		//get employee id
 		var employeeId = $(this).val();
-		var confirmDelete = false;
 
 		//get delete confirmation from user
 		$.confirm({
 			icon: 'fa fa-warning',
-			title: 'font-awesome',
+			title: 'Delete',
 			content: "Confirm to delete student? ID: "+employeeId,
 			buttons: {
 				confirm: function () {
-					confirmDelete = true;
+					event.preventDefault()
+					//send xml request to delete member
+					$.ajax({
+						url:'/deleteEmp',
+						type: "POST",
+						data :{"delete":"true", "employee_id":employeeId},
+						/* display delete result after recieved xml response */
+						// [ Ajax responses ]
+						// 0 - Student_id does not exists
+						// 1 - Deletion successful
+						// 2 - Error occured
+					success:function(data){
+						switch (data.response) {
+							case '0':
+							alert("Member does not exist");
+							break;
+							case '1':
+							$("tr#"+employeeId).fadeOut(1000);
+							console.log('Deleted: '+employeeId);
+							break;
+							case '2':
+							alert("Sorry, something went wrong");
+							break;
+							default:
+							console.log('Something went wrong');
+							break;
+						}
+					}
+			});
 				},
 				cancel: function () {
-					confirmDelete = false;
+					
 				}
 			}
 		});
 
 		// var confirmDelete = confirm("Confirm to delete student? ID: "+employeeId);
-		event.preventDefault()
-		if(confirmDelete === true){
-			//send xml request to delete member
-			$.ajax({
-				url:'/deleteEmp',
-				type: "POST",
-				data :{"delete":"true", "employee_id":employeeId},
-				/* display delete result after recieved xml response */
-				// [ Ajax responses ]
-				// 0 - Student_id does not exists
-				// 1 - Deletion successful
-				// 2 - Error occured
-				success:function(data){
-					switch (data.response) {
-						case '0':
-							alert("Member does not exist");
-							break;
-						case '1':
-							$("tr#"+employeeId).fadeOut(1000);
-							console.log('Deleted: '+employeeId);
-							break;
-						case '2':
-							alert("Sorry, something went wrong");
-							break;
-						default:
-							console.log('Something went wrong');
-							break;
-					}
-				}
-			});
-		}
+		// if(confirmDelete === true){
+			
+		// }
 	});
 	
 
