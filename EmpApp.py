d137763913a9f8a79e30664867c4401bc92c9d28
evilsimon789 <58799463+evilsimon789@users.noreply.github.com>
2021-12-08 14:22:12 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -27,9 +27,9 @@ def home():
     #creating variable for connection
     cursor=db_conn.cursor(pymysql.cursors.DictCursor)
 
-    sql = "SELECT E.employeeId, E.firstName, E.lastName, E.gender, E.email, E.phoneNo, E.location, E.hireDate, P.positionName, D.departmentName \ 
-        from ((employee E INNER JOIN position P ON E.positionId = P.positionId)\ 
-            INNER JOIN department D ON E.departmentId = D.departmentId"
+    sql = "SELECT E.employeeId, E.firstName, E.lastName, E.gender, E.email, E.phoneNo, E.location, E.hireDate, P.positionName, D.departmentName " \ 
+        "from ((employee E INNER JOIN position P ON E.positionId = P.positionId)" \
+            "INNER JOIN department D ON E.departmentId = D.departmentId"
 
     #executing query
     cursor.execute()
