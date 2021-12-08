833573f1f4e22f336d92756287dbd47216edfb26
evilsimon789 <58799463+evilsimon789@users.noreply.github.com>
2021-12-08 17:47:28 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, flask
 from pymysql import connections
 import os
 import boto3
