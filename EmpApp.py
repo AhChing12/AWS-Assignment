6bfe4e0239847a082a8424cf59513fe1ba4121f1
evilsimon789 <58799463+evilsimon789@users.noreply.github.com>
2021-12-08 16:47:44 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -40,7 +40,7 @@ def home():
 
 @app.route("/deleteEmp", methods=['POST'])
 def deleteEmp():
-    print(request.form['delete'], file=sys.stderr)
+    print(request.form['delete'])
     return 1;
 
 
