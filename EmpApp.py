fd4da306c2e515775eddb6e10a7c80d3cc4460d4
evilsimon789 <58799463+evilsimon789@users.noreply.github.com>
2021-12-10 14:52:55 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -102,14 +102,14 @@ def attendance():
     # sql = "SELECT E.employeeId, E.firstName, E.lastName, E.gender, E.email, E.phoneNo, E.location, E.hireDate, P.positionName, D.departmentName from employee E INNER JOIN position P ON E.positionId = P.positionId INNER JOIN department D ON E.departmentId = D.departmentId"
 
     #executing query
-    cursor.execute(sql, datetime.strptime(datetime.now(), '%Y-%m-%d'))
+    cursor.execute(sql, datetime.now().strptime('%Y-%m-%d'))
 
     #fetching all records from database
     data=cursor.fetchall()
 
     print(data)
 
-    print(datetime.strptime(datetime.now(), '%Y-%m-%d'))
+    print(datetime.now().strptime('%Y-%m-%d'))
 
     return render_template('Attendance.html', data=data)
 
